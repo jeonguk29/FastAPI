77d2cf1d4d9dc4004911662a5acc2203f96c6c14
정정욱 <jeonguk29@naver.com>
2025-09-30 20:06:57 +0900

    day2: 고급 타입 힌트 사용
    typing 모듈의 List , Dict 등을 사용하여 복잡한 데이터 구조 표현, Query 는 쿼리 매개변수의 기본값 설정 및 유효성 검사에 사용됩니다. Query([) 는 매개변수가 필수가 아니며, 기본값 으로 빈 리스트를 설정합니다.
    List 타입 힌트는 반드시 Query0 와 함께 사용해야 합니다

--- 00_START/main.py
+++ 00_START/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import List, Dict
 
 app = FastAPI()
 
-# 타입 힌트 사용
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id,}
+'''
+Query 는 쿼리 매개변수의 기본값 설정 및 유효성 검사에 사용됩니다. Query([) 는 매개변수가 필수가 아니며, 기본값 으로 빈 리스트를 설정합니다.
+List 타입 힌트는 반드시 Query0 와 함께 사용해야 합니다
 
-@app.get("/getdata/")
-def read_items(data: str = "funcoding"):
-    return {"data": data}
+async를 씀으로서 비동기를 명시 await과 같이써야 비동기 작업이 가능함 이것만으로는 비동기 작업을 할 수 있는건 아님
+관습적으로 쓰는 경우도 종종 있음
+'''
+@app.get("/items/")
+async def read_items(q: List[int] = Query([])):
+    return {"q": q}
+
+@app.post("/create-item/")
+async def create_item(item: Dict[str, int]):
+    return item
