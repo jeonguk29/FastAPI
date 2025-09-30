905591be7a039cc133f4d8208ecf01d359c40739
정정욱 <jeonguk29@naver.com>
2025-09-30 18:18:20 +0900

    day2: 기본 타입 힌트 사용
    타입 힌트(Type Hint)는 변수나 함수의 예상되는 데이터 타입을 명시적으로 표시하는 프로그래밍 기술입니다. FastAPI는 이를 활용하여 요청의 유효성을 검증하고, 적절한 데이터 처리를 도와줍니다

--- 00_START/main.py
+++ 00_START/main.py
@@ -2,31 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI"}
-
-# http://127.0.0.1:8000/items/
-'''
-1. 경로 매개변수 (Path Parameter)
-
-URL 경로의 일부로 값을 전달
-
-특정 리소스를 식별할 때 주로 사용
-'''
+# 타입 힌트 사용
 @app.get("/items/{item_id}")
-def read_item(item_id):
-    return {"item_id": item_id}
+def read_item(item_id: int):
+    return {"item_id": item_id,}
 
-
-# http://127.0.0.1:8000/items/?skip=5&limit=12
-'''
-2. 쿼리 파라미터 (Query Parameter)
-
-URL 경로 끝에 ?key=value 형식으로 붙는 값
-
-옵션/필터링/검색 등에 주로 사용
-'''
-@app.get("/items/")
-def read_items(skip = 0, limit = 10):
-    return {"skip": skip, "limit": limit}
+@app.get("/getdata/")
+def read_items(data: str = "funcoding"):
+    return {"data": data}
