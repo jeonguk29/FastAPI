d604d133f815c860712b2dbace0ca45ede876dba
정정욱 <jeonguk29@naver.com>
2025-10-01 16:12:41 +0900

    day4: FastAPI 응답 클래스 이해

--- 00_START/main.py
+++ 00_START/main.py
@@ -1,52 +1,25 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Union
-from typing import List
+from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse, PlainTextResponse
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    description: str = None
-    price: float
-
-def get_item_from_db(id):
-    # 매우 간단한 아이템 반환
-    return {
-        "name": "Simple Item", 
-        "description": "A simple item description", 
-        "price": 50.0,
-        "dis_price": 45.0
-    }
-  
-@app.get("/items/{item_id}", response_model=Item) # 반환 모델로 Item을 지정 그려면 반환값에 dis_price는 안들어감 
-def read_item(item_id: int):
-    item = get_item_from_db(item_id)
-    return item
+@app.get("/json", response_class=JSONResponse)
+def read_json():
+    return {"msg": "This is JSON"}
 
 
-
-# 유니온 응답 모델 여러 가능한 모델 중 하나를 반환 할 수 있는 유니온 타입을 사용하는 모델
-
-class Cat(BaseModel):
-    name: str
-
-class Dog(BaseModel):
-    name: str
-
-@app.get("/animal/", response_model=Union[Cat, Dog])
-async def get_animal(animal: str): # 쿼리 파라미터를 받아서 알맞은 모델로 반환
-    if animal == "cat":
-        return Cat(name="Whiskers") #모델에 멤버 변수이름이랑 맞아야함 안맞으면 오류남
-    else:
-        return Dog(name="Fido")
+@app.get("/html", response_class=HTMLResponse)
+def read_html():
+    return "<h1>This is HTML</h1>"
 
 
-# 리스트 응답 모델 예시
+@app.get("/text", response_class=PlainTextResponse)
+def read_text():
+    return "This is Plain Text"
 
-class Item(BaseModel):
-    name: str
-
-@app.get("/items/", response_model=List[Item])
-async def get_items():
-    return [{"name": "Item 1"}, {"name": "Item 2"}]
+@app.get("/redirect")
+def read_redirect(): #해당 주소로 입력이 들어오면 아래 API로 이동해라 => 최종 보여지는건 /text에 해당하는 반환값
+    # 기존 url을 최신 url로 바꿀때 등 다양한 목적으로 사용
+    return RedirectResponse(url="/text")
