0b5af2de783e3afff714e9623a27971a00172248
정정욱 <jeonguk29@naver.com>
2025-10-02 13:34:10 +0900

    day5: FastAPI 리퀘스트와 Body 클래스

--- 00_START/main.py
+++ 00_START/main.py
@@ -1,30 +1,44 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
-@app.get("/info/")
-def read_info(info: str = Query(None, description="정보를 입력해 주세요.")):
-    return {"info": info}
+@app.post("/items/")
+def create_item(item: dict = Body(...)): #보낼때 해당 바디에 이 값이 필수다라고 선언 스웨거에서도 확인 가능함
+    return {"item": item}
 
-@app.get("/items/")
-def read_items(
-    # regex는 지금 정규 표현식이 들어가있 맨앞부터 맨뒤는 알파뱃만 가능하고 +는 하나 이상을 의미함
-    string_query: str = Query(default="default value", min_length=2, max_length=5, regex="^[a-zA-Z]+$", title="String Query", example="abc"),
-    number_query: float = Query(default=1.0, ge=0.5, le=10.5, title="Number Query", example=5.5)
-    # ge는 >= le는 <=를 이야기함 
+#Body 클래스는 Post, Put 방식에 적합함 
+
+
+@app.post("/advanced_items/")
+def create_advanced_item(item: dict = Body(
+    default=None, 
+    example={"key": "value"}, 
+    alias="item_alias", 
+    title="Sample Item", 
+    description="This is a sample item", 
+    deprecated=False)):
+    return {"item": item}
+
+
+@app.post("/advanced_items_2para/")
+def create_advanced_item(
+    item: dict = Body(
+        default=None,
+        example={"key": "value"},
+        alias="item_alias",
+        title="Sample Item",
+        description="This is a sample item",
+        deprecated=False
+    ),
+    additional_info: dict = Body(
+        default=None,
+        example={"info_key": "info_value"},
+        title="Additional Info",
+        description="This is some additional information about the item",
+        deprecated=False
+    )
 ):
     return {
-        "string_query_handled": string_query,
-        "number_query_handled": number_query
+        "item_alias": item,
+        "additional_info": additional_info
     }
-
-'''
-이렇게 요청에서 파라미터들을 제어 할 수 있는 것이 Query 클래스
-HTTP Request
-- Packet: header/body
-- header options
- - URL parameters(in GET)
-
-body
- - parameters(in Post)
-'''
