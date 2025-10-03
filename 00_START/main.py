849eadab0b2264081ee36c95192649384b3f8d94
정정욱 <jeonguk29@naver.com>
2025-10-03 14:37:18 +0900

    day5: FastAPI 템플릿과 Jinja2 기본

--- 00_START/main.py
+++ 00_START/main.py
@@ -1,17 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates") #이걸 사실 안써도 됨 기본 문법으로 선언 되어 있음
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    try:
-        if item_id < 0:
-            raise ValueError("음수는 허용되지 않습니다.")
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e)) #제일 중요한건 에러 코드와 에러 메시지임
-    
-@app.get("/items2/{item_id}")
-def read_item(item_id: int):
-    if item_id == 42:
-        raise HTTPException(status_code=404, detail="Item not found") # try-except 없이 명시적으로 에러 처리, 에러를 보낼때는 raise를 붙임
-    return {"item_id": item_id}
+@app.get("/")
+def read_root(request: Request): #Request는 사용자가 요청할때 헤더부터 바디까지 가지고 있는 전체 데이터
+    return templates.TemplateResponse("index.html", {"request": request, "username": "JeongUk"})
