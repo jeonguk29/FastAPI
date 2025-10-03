6e065a396f490811a2c309ddbc769f304c990e6d
정정욱 <jeonguk29@naver.com>
2025-10-03 14:59:51 +0900

    day6: Jinja2 기본 문법1

--- 00_START/main.py
+++ 00_START/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates") #이걸 사실 안써도 됨 기본 문법으로 선언 되어 있음
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def read_root(request: Request): #Request는 사용자가 요청할때 헤더부터 바디까지 가지고 있는 전체 데이터
-    return templates.TemplateResponse("index.html", {"request": request, "username": "JeongUk"})
+@app.get("/user/{username}")
+def get_user(request: Request, username: str):
+    return templates.TemplateResponse("index.html", {"request": request, "username": username})
+  
+@app.get("/safe")
+def read_root_safe(request: Request):
+    my_variable_with_html = "<h1>Hello, FastAPI!</h1>"
+    return templates.TemplateResponse("index_with_safe.html", {"request": request, "my_variable_with_html": my_variable_with_html})
+
+@app.get("/greet")
+def greeting(request: Request, time_of_day: str):
+    return templates.TemplateResponse("conditional.html", {"request": request, "time_of_day": time_of_day})
