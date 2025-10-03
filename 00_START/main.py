49e4e4b471b3313da241af2e6dfc12564af2b4ad
정정욱 <jeonguk29@naver.com>
2025-10-03 15:28:42 +0900

    day6: Jinja2 기본 문법2 (반복문, 상속)

--- 00_START/main.py
+++ 00_START/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from typing import List
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -16,3 +17,20 @@ def read_root_safe(request: Request):
 @app.get("/greet")
 def greeting(request: Request, time_of_day: str):
     return templates.TemplateResponse("conditional.html", {"request": request, "time_of_day": time_of_day})
+
+@app.get("/items")
+def read_items(request: Request):
+    my_items = ["apple", "banana", "cherry"]
+    return templates.TemplateResponse("loop.html", {"request": request, "items": my_items})
+
+
+# http://127.0.0.1:8000/dynamic_items/?item_list=사과,바나나,체리
+@app.get("/dynamic_items/")
+def dynamic_items(request: Request, item_list: str = ""):
+    items = item_list.split(",")
+    return templates.TemplateResponse("loop.html", {"request": request, "items": items})
+
+@app.get("/inherit")
+def template_inherit(request: Request):
+    my_text = "FastAPI와 Jinja2를 이용한 예시입니다."
+    return templates.TemplateResponse("inherit.html", {"request": request, "text": my_text})
